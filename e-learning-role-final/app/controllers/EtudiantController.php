d348cf4aaa74bcd7b51bf28f4398f30656f1fbdc
MelissaAb-tech <melissameli2322004@gmail.com>
2025-04-21 20:21:44 +0200

    Delete e-learning-role-final directory

--- e-learning-role-final/app/controllers/EtudiantController.php
+++
@@ -1,9 +0,0 @@
-<?php
-class EtudiantController extends Controller
-{
-    public function dashboard()
-    {
-        $cours = $this->model('Cours')->getAll();
-        $this->view('etudiant/dashboard', ['cours' => $cours]);
-    }
-}
--- e-learning-role-final/app/controllers/HomeController.php
+++
@@ -1,9 +0,0 @@
-<?php
-class HomeController extends Controller
-{
-    public function index()
-    {
-        $cours = $this->model('Cours')->getAll();
-        $this->view('home/index', ['cours' => $cours]);
-    }
-}
